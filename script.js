0aca6ef922fb4e6c64876a79f6520eb1b0f0e8e5
tkapustka <tylerkapustka@gmail.com>
2022-03-23 18:19:43 -0500

    innerText color codes

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 
+const button = document.getElementById('color')
+// const colorHex = document.querySelector('colorText')
+const colorHex = document.querySelector('colorText')
+
 
 button.addEventListener(`click`, async () => {
     let response = await axios.get('https://x-colors.herokuapp.com/api/random')
     console.log(response)
-    let colorAppear = response.data.hex
-    console.log(colorAppear)
-    // colorHex.innerHTML = colorAppear
-    document.body.style.background = colorAppear
+    let colorAppearHex = response.data.hex
+    let colorAppearRgb = response.data.rgb
+    let colorAppearHsl = response.data.hsl
+    console.log(colorAppearHex)
+    document.body.style.background = colorAppearHex
+    // colorHex.innerText(colorAppear)
+    document.getElementById('colorTextHex').innerText = colorAppearHex
+    document.getElementById('colorTextRgb').innerText = colorAppearRgb
+    document.getElementById('colorTextHsl').innerText = colorAppearHsl
 })
+
